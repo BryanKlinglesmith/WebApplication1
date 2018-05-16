cd68e94b0594a59ebbb7f1877dc88944c723ec10
bryan klinglesmith <b.klinglesmith@yahoo.com>
2018-05-16 19:00:54 -0400

    initial commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Car car = new Car()
+            {
+                Brand = "Bmw",
+                Color = "Black"
+            };
+
+            return View(car);
+        }
+    }
+}
